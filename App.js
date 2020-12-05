f992bd4b7f0652549009d10058b7bd9a923b04f0
Wojciech Bożek <bozek.wojciech@gmail.com>
2020-12-05 13:22:54 +0100

    Step 2 Simple animation

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import React, { useState } from "react";
 import Slider from "react-native-smooth-slider";
+
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated";
 import { AppRegistry, StyleSheet, View, Text } from "react-native";
 
 const SmileApp = () => {
+  const lvl = useSharedValue(1);
   const [size, setSize] = useState(1);
 
+  const style = useAnimatedStyle(() => {
+    return {
+      fontSize: 10 + 4 * lvl.value,
+    };
+  });
   return (
     <View style={styles.container}>
       <Slider
         value={size}
         useNativeDriver={true}
-        onValueChange={(value) => setSize(value)}
+        onValueChange={(value) => (lvl.value = value)}
         maximumValue={5}
         minimumValue={1}
         step={1}
       />
-      <Text>Value: {size}</Text>
+      <Animated.Text style={style}>Something</Animated.Text>
     </View>
   );
 };
