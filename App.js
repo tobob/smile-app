0d70527b64c7ba213763eab9a12acc9632a2ce79
Wojciech Bożek <bozek.wojciech@gmail.com>
2020-12-05 13:06:30 +0100

    Step 1 working project

--- App.js
+++ App.js
@@ -1,47 +1,38 @@
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  Easing,
-} from "react-native-reanimated";
-import { View, Button } from "react-native";
 import React from "react";
+import Slider from "react-native-smooth-slider";
+import { AppRegistry, StyleSheet, View, Text } from "react-native";
 
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
-
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
+class SliderExample extends React.Component {
+  state = {
+    value: 1,
   };
 
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
-  });
-
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
-    </View>
-  );
+  render() {
+    return (
+      <View style={styles.container}>
+        <Slider
+          value={this.state.value}
+          useNativeDriver={true}
+          onValueChange={(value) => this.setState({ value })}
+          maximumValue={5}
+          minimumValue={1}
+          step={1}
+        />
+        <Text>Value: {this.state.value}</Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 60,
+    padding: 40,
+    marginLeft: 10,
+    marginRight: 10,
+    alignItems: "stretch",
+    justifyContent: "center",
+  },
+});
+
+export default SliderExample;
