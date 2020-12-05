ca07a3b0aab2321d12e08adbc6303a5b27dd7cda
Wojciech Bożek <bozek.wojciech@gmail.com>
2020-12-05 13:32:56 +0100

    Step 3 Simple Text Animation

--- App.js
+++ App.js
@@ -1,32 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import Slider from "react-native-smooth-slider";
 
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
+  withSpring,
 } from "react-native-reanimated";
 import { AppRegistry, StyleSheet, View, Text } from "react-native";
 
 const SmileApp = () => {
-  const lvl = useSharedValue(1);
-  const [size, setSize] = useState(1);
+  const lvl = useSharedValue(3);
+  const [size, setSize] = useState(3);
+
+  useEffect(() => {
+    lvl.value = size;
+  }, [size]);
+
+  const text = useMemo(() => {
+    return {
+      1: "worst",
+      2: "don't like",
+      3: "neutral",
+      4: "like",
+      5: "love",
+    }[size];
+  }, [size]);
 
   const style = useAnimatedStyle(() => {
     return {
       fontSize: 10 + 4 * lvl.value,
+      fontWeight: (400 + 100 * (lvl.value - 1)).toString(),
     };
   });
+
   return (
     <View style={styles.container}>
       <Slider
         value={size}
         useNativeDriver={true}
-        onValueChange={(value) => (lvl.value = value)}
+        onValueChange={(value) => setSize(value)}
         maximumValue={5}
         minimumValue={1}
         step={1}
       />
-      <Animated.Text style={style}>Something</Animated.Text>
+      <Animated.Text style={[styles.text, style]}>{text}</Animated.Text>
     </View>
   );
 };
@@ -39,6 +56,12 @@ const styles = StyleSheet.create({
     marginRight: 10,
     alignItems: "stretch",
     justifyContent: "center",
+  },
+  text: {
+    textAlign: "center",
+  },
+  value: {
+    textAlign: "center",
   },
 });
 
